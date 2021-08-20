810ebcccfc974a91713dfe7d5228a3dd47d288b2
UnknownRori <akbar.hendra34@gmail.com>
2021-08-20 09:18:09 +0700

    update database

--- database/phone-market-revision.sql
+++ database/phone-market-revision.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 19, 2021 at 09:13 AM
+-- Generation Time: Aug 20, 2021 at 04:17 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -35,7 +35,8 @@ CREATE TABLE `buy_request` (
   `total_requested` int(11) NOT NULL,
   `buy_timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
   `delivery_timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
-  `read_status` tinyint(1) NOT NULL
+  `vendor_read_status` tinyint(1) NOT NULL,
+  `delivery_product_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -67,51 +68,33 @@ CREATE TABLE `feature` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mail`
---
-
-CREATE TABLE `mail` (
-  `id` int(11) NOT NULL,
-  `writer_id` int(11) NOT NULL,
-  `receiver_id` int(11) NOT NULL,
-  `content` varchar(2048) NOT NULL,
-  `read_status` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `product`
 --
 
 CREATE TABLE `product` (
-  `id` int(11) NOT NULL,
+  `prod_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `product_name` varchar(255) NOT NULL,
+  `photo_name` varchar(255) NOT NULL,
   `price` int(11) NOT NULL,
   `stock` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL
+  `description` varchar(255) NOT NULL,
+  `warned_status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `product`
 --
 
-INSERT INTO `product` (`id`, `user_id`, `product_name`, `price`, `stock`, `description`) VALUES
-(1, 1, 'test_product', 123, 345, 'asd');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `purchase_history`
---
-
-CREATE TABLE `purchase_history` (
-  `id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `purchased_time` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `product` (`prod_id`, `user_id`, `product_name`, `photo_name`, `price`, `stock`, `description`, `warned_status`) VALUES
+(1, 1, 'test_product', '', 123, 345, 'asd', 0),
+(2, 4, 'Yolo', '', 0, 0, 'default', 0),
+(3, 4, 'A', '', 0, 0, 'default', 0),
+(5, 4, 'Q', '', 0, 0, 'default', 0),
+(6, 4, 'asd', '', 1, 1, 'as', 0),
+(7, 1, 'as2', '', 1, 1, 'as', 0),
+(8, 1, 'Cat', '', 0, 0, 'default', 0),
+(9, 4, 'urra', '', 0, 0, 'default', 0);
 
 -- --------------------------------------------------------
 
@@ -132,8 +115,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `admin`, `vendor`) VALUES
-(1, 'UnknownRori', '$2y$10$zQRuqe3wjyCTgCnkyxIx7OLpk8F7qrW5wflAdW/yndbMGfLndFIka', 1, 1),
-(4, 'Akashi', '$2y$10$qqFyLeHnO891s.EKf8ahfuVM1vW7Iyw0xMuvhDWLG.PfkYZ0XazRa', 0, 1);
+(1, 'UnknownRori', '$2y$10$qC1QXBxixDaQ.fL6Rmaf.OePOexMWTiXw2Y6q45RVB3xTg5yY85Gu', 1, 1),
+(4, 'Akashi', '$2y$10$tiPFid4Ovw43aJGmwxfUou8wbL6JmJgRZjzE8NJeHtxbGeLG8Wz1K', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -176,28 +159,12 @@ ALTER TABLE `feature`
   ADD KEY `product_id` (`product_id`);
 
 --
--- Indexes for table `mail`
---
-ALTER TABLE `mail`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `writer` (`writer_id`),
-  ADD KEY `reader` (`receiver_id`);
-
---
 -- Indexes for table `product`
 --
 ALTER TABLE `product`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`prod_id`),
   ADD UNIQUE KEY `product_name` (`product_name`),
   ADD KEY `users_id` (`user_id`);
-
---
--- Indexes for table `purchase_history`
---
-ALTER TABLE `purchase_history`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `product_id` (`product_id`),
-  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `users`
@@ -237,22 +204,10 @@ ALTER TABLE `feature`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `mail`
---
-ALTER TABLE `mail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `purchase_history`
---
-ALTER TABLE `purchase_history`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -274,28 +229,21 @@ ALTER TABLE `view_history`
 -- Constraints for table `buy_request`
 --
 ALTER TABLE `buy_request`
-  ADD CONSTRAINT `product_request` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `product_request` FOREIGN KEY (`product_id`) REFERENCES `product` (`prod_id`),
   ADD CONSTRAINT `users_request` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `comment`
 --
 ALTER TABLE `comment`
-  ADD CONSTRAINT `products_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `products_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`prod_id`),
   ADD CONSTRAINT `user_id` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `feature`
 --
 ALTER TABLE `feature`
-  ADD CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
-
---
--- Constraints for table `mail`
---
-ALTER TABLE `mail`
-  ADD CONSTRAINT `reader` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `writer` FOREIGN KEY (`writer_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`prod_id`);
 
 --
 -- Constraints for table `product`
@@ -304,17 +252,10 @@ ALTER TABLE `product`
   ADD CONSTRAINT `users_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 --
--- Constraints for table `purchase_history`
---
-ALTER TABLE `purchase_history`
-  ADD CONSTRAINT `prod` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
-  ADD CONSTRAINT `user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
 -- Constraints for table `view_history`
 --
 ALTER TABLE `view_history`
-  ADD CONSTRAINT `product` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+  ADD CONSTRAINT `product` FOREIGN KEY (`product_id`) REFERENCES `product` (`prod_id`),
   ADD CONSTRAINT `users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
